7b6cc7f4224cd1debf56339883a1a470a82b2a83
Switchblade4703 <156360989+Switchblade4703@users.noreply.github.com>
2024-10-06 14:01:34 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using ex4_1MovieList.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ex4_1MovieList.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
